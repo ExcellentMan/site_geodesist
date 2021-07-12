50a334350b73c7c35428081d9051a789b8c5af1f
Kirill <excellentmanpro@gmail.com>
2021-07-12 22:00:23 +0300

    add animation logo and section "about"

--- anim.js
+++ anim.js
@@ -1,24 +1,29 @@
-function isVisible(elem){
-  var docViewTop = $(window).scrollTop();
-  var docViewBottom = docViewTop + $(window).height();
-
-  var elemTop = $(elem).offset().top;
-  var elemBottom = elemTop + $(elem).height();
-
-  return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-}
-
-$(window).scroll(function() {
-console.log('scroll')
-$('.fadeInBlock').each(function(i) {
-  if (isVisible(this)) {
-    $(this).animate({
-      'opacity': '1'
-    }, 1000);
-  } else {
-    $(this).css({
-      'opacity': '0'
-    });
+$(window).scroll(function(){
+  var offset = $('.about').offset(),
+      offsetwb = $('.white + .black').offset();
+  if ($(this).scrollTop() > offset.top && $(this).scrollTop() < offsetwb.top) {
+    $('.icon').css('background-color','black');
+  }
+  else {
+    $('.icon').css('background-color','white');
   }
 });
-});
+
+
+const animateCSS = (element, animation, prefix = 'animate__') =>
+  // We create a Promise and return it
+  new Promise((resolve, reject) => {
+    const animationName = `${prefix}${animation}`;
+    const node = document.querySelector(element);
+
+    node.classList.add(`${prefix}animated`, animationName);
+
+    // When the animation ends, we clean the classes and resolve the Promise
+    function handleAnimationEnd(event) {
+      event.stopPropagation();
+      node.classList.remove(`${prefix}animated`, animationName);
+      resolve('Animation ended');
+    }
+
+    node.addEventListener('animationend', handleAnimationEnd, {once: true});
+  });
